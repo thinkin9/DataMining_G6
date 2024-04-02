ac3c0158eb23404a1dbe0ca1fa46d44e5d0ed7fc
Hyunju Kim <sherlockkhj@naver.com>
2024-04-03 03:33:50 +0900

    final-naive

--- FPTree_HJ/src/FPTree/ItemCount.java
+++ FPTree_HJ/src/FPTree/ItemCount.java
@@ -1,5 +1,7 @@
 package FPTree;
 
+import java.util.List;
+
 public class ItemCount {
     public String name;
     public int count;
@@ -7,5 +9,16 @@ public class ItemCount {
     public ItemCount(String name, int count){
         this.name = name;
         this.count = count;
+    }
+    public String getItems() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String toString() {
+        return "[" + name + "] - Count: " + count;
     }
 }
