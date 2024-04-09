89b8160ff829523c503c4790138f359c027ecd96
zxzx1825@unist.ac.kr <53069520+thinkin9@users.noreply.github.com>
2024-04-09 11:52:20 +0900

    results

--- eval.sh
+++ eval.sh
@@ -5,6 +5,7 @@ APRIORI_FILE="A1_G6_t1"
 FPGROWTH_FILE="A1_G6_t2"
 
 DATASET="T2_N100_D10K_Corr5.csv T5_N100_D10K_Corr5.csv T10_N1K_D10K_Corr5.csv T10_N100_D1K_Corr5.csv T10_N100_D10K_Corr5.csv T10_N100_D100K_Corr5.csv T20_N100_D10K_Corr5.csv"
+MINSUP="0.01 0.05 0.1"
 
 echo "1. Compile Apriori File: $APRIORI_FILE.java"
 javac $APRIORI_FILE.java
@@ -17,19 +18,38 @@ echo "2. Done."
 echo ""
 
 echo "3. RUN with Groceries.csv"
-echo "RESULT: java $APRIORI_FILE ./database/groceries.csv 0.15"
-java $APRIORI_FILE ./database/groceries.csv 0.15
+echo "java $APRIORI_FILE ./database/groceries.csv 0.15"
+echo "java $APRIORI_FILE ./database/groceries.csv 0.15" > result_Apriori.txt
+java $APRIORI_FILE ./database/groceries.csv 0.15 >> result_Apriori.txt
+
+echo "java $FPGROWTH_FILE ./database/groceries.csv 0.15"
+echo "java $FPGROWTH_FILE ./database/groceries.csv 0.15" > result_FPGrowth.txt
+java $FPGROWTH_FILE ./database/groceries.csv 0.15 >> result_FPGrowth.txt
 echo ""
-echo "RESULT: java $FPGROWTH_FILE ./database/groceries.csv 0.15"
-java $FPGROWTH_FILE ./database/groceries.csv 0.15
-echo ""
+echo "3. Done."
 
 echo "4. RUN with our data"
 for DATA in $DATASET; do
-    echo "RESULT: java $APRIORI_FILE ./database/$DATA 0.01"
-    java $APRIORI_FILE ./database/$DATA 0.01
-    echo ""
-    echo "RESULT: java $FPGROWTH_FILE ./database/$DATA 0.01"
-    java $FPGROWTH_FILE ./database/$DATA 0.01
+    echo "java $APRIORI_FILE ./database/$DATA 0.01"
+    echo "java $APRIORI_FILE ./database/$DATA 0.01" >> result_Apriori.txt
+    java $APRIORI_FILE ./database/$DATA 0.01 >> result_Apriori.txt
+
+    echo "java $FPGROWTH_FILE ./database/$DATA 0.01"
+    echo "java $FPGROWTH_FILE ./database/$DATA 0.01" >> result_FPGrowth.txt
+    java $FPGROWTH_FILE ./database/$DATA 0.01 >> result_FPGrowth.txt
     echo ""
 done
+echo "4. Done."
+
+echo "5. RUN with various min_sup"
+for _MINSUP in $MINSUP; do
+    echo "java $APRIORI_FILE ./database/T10_N100_D10K_Corr5.csv $_MINSUP"
+    echo "java $APRIORI_FILE ./database/T10_N100_D10K_Corr5.csv $_MINSUP" >> result_Apriori.txt
+    java $APRIORI_FILE ./database/T10_N100_D10K_Corr5.csv $_MINSUP >> result_Apriori.txt
+    
+    echo "java $FPGROWTH_FILE ./database/T10_N100_D10K_Corr5.csv $_MINSUP"
+    echo "java $FPGROWTH_FILE ./database/T10_N100_D10K_Corr5.csv $_MINSUP" >> result_FPGrowth.txt
+    java $FPGROWTH_FILE ./database/T10_N100_D10K_Corr5.csv $_MINSUP >> result_FPGrowth.txt
+    echo ""
+done
+echo "5. Done."
