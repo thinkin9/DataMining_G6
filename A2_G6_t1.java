19d1b98467f8beef8948bdb3ff9faed092483c8f
kimjunseo <jeff14916@gmail.com>
2024-05-21 19:49:33 +0900

    init k-means

--- A2_G6_t1.java
+++ A2_G6_t1.java
@@ -8,13 +8,17 @@ public class A2_G6_t1 {
         // Argument Parsing: java A2_G2_t1 ./database/artd-31.csv 15
 
         String fileName = args[0];
-        int minPts = Integer.parseInt(args[1]);
-        double eps = Double.parseDouble(args[2]);
-        
+        int Pts = 1;
+        if (args.length == 2) {
+            Pts = Integer.parseInt(args[1]);
+        } else {
+            Pts = 10; // have to estimate
+        }
+
         long startTime = System.currentTimeMillis();
 
         long endTime = System.currentTimeMillis();
-        System.out.println("A2_G6_t1 Processing Execution time: " + (endTime - startTime)/1000.0);
+        System.out.println("A2_G6_t1 Processing Execution time: " + (endTime - startTime) / 1000.0);
 
     }
 }
